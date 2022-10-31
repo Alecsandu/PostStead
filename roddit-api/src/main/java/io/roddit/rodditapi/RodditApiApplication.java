e61900b3a5abf6915512654354e7ce47ecec5cfc
Alecsandu <45858741+Alecsandu@users.noreply.github.com>
2022-10-31 22:05:40 +0200

    Added initial api project

---
+++ roddit-api/src/main/java/io/roddit/rodditapi/RodditApiApplication.java
@@ -0,0 +1,13 @@
+package io.roddit.rodditapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodditApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RodditApiApplication.class, args);
+    }
+
+}
---
+++ roddit-api/src/test/java/io/roddit/rodditapi/RodditApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.roddit.rodditapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodditApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
